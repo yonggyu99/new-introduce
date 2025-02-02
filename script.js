eff1dd8018070b328620ee0150163e82df0c6946
Yonggyu Park <yoyo5411@naver.com>
2025-02-02 18:38:44 +0900

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const travelImg = document.querySelector(".travel-img");
+
+travelImg.addEventListener("click", () => {
+    if(travelImg.src.includes("like1.jpg")){
+        travelImg.src = "images/like2.jpg";
+    }
+    else if(travelImg.src.includes("like2.jpg")){
+        travelImg.src = "images/like3.jpg";
+    }
+    else{
+        travelImg.src = "images/like1.jpg";
+    }
+});
+
 
 document.querySelector('.profile-card').addEventListener('click', function () {
     this.classList.toggle('is-flipped');
 });
+
