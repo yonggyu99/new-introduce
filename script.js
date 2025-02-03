e07d545f54397b02c45e8d1f068564d8858ae427
Yonggyu Park <yoyo5411@naver.com>
2025-02-03 21:03:20 +0900

    add video

--- script.js
+++ script.js
@@ -15,6 +15,28 @@ travelImg.addEventListener("click", () => {
         travelImg.src = "images/like1.jpg";
     }
 });
+
+const modal = document.getElementById("videoModal");
+const closeModal = document.querySelector(".close");
+const youtubeVideo = document.getElementById("youtubeVideo");
+
+document.querySelector(".dance").addEventListener("click", function () {
+    modal.style.display = "flex";
+    youtubeVideo.src = "https://www.youtube.com/embed/H9XhBnkno5w"; // 유튜브 영상 URL
+});
+
+closeModal.addEventListener("click", function () {
+    modal.style.display = "none";
+    youtubeVideo.src = ""; 
+});
+
+window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+        youtubeVideo.src = "";
+    }
+});
+
 };
 
 if(currentPage.includes("index.html")){
